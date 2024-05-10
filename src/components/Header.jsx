7dfa212cec99feb8d6ef91e814a61c85ec90b36b
harshkchoudhary <harsh.c@libratherm.com>
2024-05-10 12:36:14 +0530

    Header, Sidebar and Topbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { useTheme } from "@emotion/react";
+import React from "react";
+import { tokens } from "../theme";
+import { Box, Typography } from "@mui/material";
+
+const Header = ({ title, subtitle }) => {
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+	return (
+		<Box mb={30}>
+			<Typography variant="h2" color={colors.grey[100]} fontWeight={"bold"} sx={{ mb: "5px" }}>
+				{title}
+			</Typography>
+			<Typography variant="h5" color={colors.greenAccent[400]}>
+				{subtitle}
+			</Typography>
+		</Box>
+	);
+};
+
+export default Header;
